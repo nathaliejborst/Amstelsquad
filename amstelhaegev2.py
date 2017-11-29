d4dba586140326206cf0739206b417dcd987566f
Dennis Broekhuizen <dennisbroekhuizen@hotmail.com>
2017-11-29 21:30:20 +0100

    Random plot first house + style update

--- amstelhaegev2.py
+++ amstelhaegev2.py
@@ -4,9 +4,7 @@
 # Description: Plots potential residential area Amstelhaege.
 
 import numpy as np
-import matplotlib.pyplot as plt
 import classes as cl
-import matplotlib.patches as patches
 from shapely.geometry import Polygon
 import math
 from datetime import datetime
@@ -23,11 +21,12 @@ def writeToFile(totalvalue):
         writer.writerow({"value": totalvalue})
 
 
-# Fixing random state for reproducibility
+# Fixing random state for reproducibility.
 # np.random.seed(2)
 
-# List of every house placed on the grid
+# List of every house placed on the grid.
 houses = []
+
 # Create instance of the grid
 grid = cl.Grid()
 
@@ -39,37 +38,43 @@ amountOfMaisons = 3 * areaVariant
 amountOfBungalows = 5 * areaVariant
 amountOfFamilyHouses = 12 * areaVariant
 
+
 # Returns true if two houses intersect.
 def Intersect(p1, p2, s1, s2):
-    if p1.intersects(p2) is True or p1.intersects(s2) is True or s1.intersects(p2) is True:
+    if p1.intersects(p2) is True or p1.intersects(s2) is True or \
+       s1.intersects(p2) is True:
         return True
     else:
         return False
 
+
 def placeFirstMaison(houseType):
-    houseType.x = houseType.freespace
-    houseType.y = houseType.freespace
+    houseType.x = np.random.randint(low=houseType.freespace,
+                                    high=grid.areaWidth -
+                                    houseType.freespace - houseType.width)
+    houseType.y = np.random.randint(low=houseType.freespace,
+                                    high=grid.areaHeight -
+                                    houseType.freespace - houseType.height)
 
     houses.append(houseType)
-    amountOfMaisons = 2
+
 
 def placeHouse(houseType):
 
     # Declare true at first, because it has to place a house.
     intersect = True
 
-    # Keeps trying to place a house until it does not intersect with another house anymore.
+    # Repeat placing house until it doesn't intersect with other houses.
     while intersect is True:
 
-
         # Generate random x and y bottom corner coordinates.
-        houseType.x = np.random.randint(low=houseType.freespace, high=grid.areaWidth -
-                              houseType.freespace - houseType.width)
-
+        houseType.x = np.random.randint(low=houseType.freespace,
+                                        high=grid.areaWidth -
+                                        houseType.freespace - houseType.width)
         houseType.y = np.random.randint(low=houseType.freespace,
-                              high=grid.areaHeight - houseType.freespace -
-                              houseType.height)
-
+                                        high=grid.areaHeight -
+                                        houseType.freespace -
+                                        houseType.height)
 
         # Declare corners and free space corners for house to be placed.
         p1 = Polygon(houseType.corners(houseType.x, houseType.y))
@@ -90,45 +95,38 @@ def placeHouse(houseType):
 
 
 placeFirstMaison(cl.House(width=11, height=10.5, freespace=6, value=610000,
-           valueUpdate=0.06))
+                          valueUpdate=0.06, color='red'))
 
-# Place water
-water = cl.House(width=144, height=40, freespace=0, value=0, valueUpdate=0, color='blue')
+# Place water.
+water = cl.House(width=144, height=40, freespace=0, value=0, valueUpdate=0,
+                 color='blue')
 placeHouse(water)
 
-# Place maisons
-for maisons in range(amountOfMaisons):
+# Place maisons.
+for maisons in range(amountOfMaisons - 1):
     maison = cl.House(width=11, height=10.5, freespace=6, value=610000,
-               valueUpdate=0.06, color='red')
+                      valueUpdate=0.06, color='red')
     placeHouse(maison)
 
-# Place bungalows
-for bungalows in range (amountOfBungalows):
+# Place bungalows.
+for bungalows in range(amountOfBungalows):
     bungalow = cl.House(width=10, height=7.5, freespace=3, value=399000,
-                 valueUpdate=0.04, color='orange')
+                        valueUpdate=0.04, color='orange')
     placeHouse(bungalow)
 
-# Place familyhouses
+# Place familyhouses.
 for familyHouses in range(amountOfFamilyHouses):
     familyHouse = cl.House(width=8, height=8, freespace=2, value=285000,
-                        valueUpdate=0.03, color='yellow')
+                           valueUpdate=0.03, color='yellow')
     placeHouse(familyHouse)
 
-# Add list of houses to grid
+# Add list of houses to grid.
 grid.housesList = houses
 
-# Plots and prints grid.
-def PlotHouses():
-    for house in houses:
-        area.add_patch(plt.Polygon(house.corners(house.x, house.y), color=house.color))
-        # area.add_patch(plt.Polygon(house.spacecorners(house.x, house.y), fill=False))
-        area.add_patch(plt.Polygon(house.extraFreeSpaceCorners(house.x, house.y), fill=False, edgecolor='green'))
 
-    plt.show()
-
-# Returns the minimum distance of the nearest house and the positon in houses[] of that house
+# Returns minimum distance of nearest house and positon in houses[] of house.
 def straightDistance(min_h, max_h, min_i, max_i):
-    # Checks distance in x or y coordinates, returns false if x or y coordinates don't match.
+    # Checks distance in x or y coord. False if x or y coordinates don't match.
     if((min_h - max_i) >= 0):
         distance = min_h - max_i
         return distance
@@ -137,6 +135,7 @@ def straightDistance(min_h, max_h, min_i, max_i):
         return distance
     else:
         return False
+
 
 def GetDistance(h):
 
@@ -153,42 +152,48 @@ def GetDistance(h):
         Ymin_i = house.Ymin(house.y)
         Ymax_i = house.Ymax(house.y)
 
-        # Get distance in y and x coordinates to other house (returns false if x or y coordinates match)
+        # Get distance in y and x coordinates to other house.
         distance_x = straightDistance(Xmin_h, Xmax_h, Xmin_i, Xmax_i)
         distance_y = straightDistance(Ymin_h, Ymax_h, Ymin_i, Ymax_i)
 
-        # Check if y coordinates match
+        # Check if y coordinates match.
         if((distance_x is not False) and (distance_y is False)):
             distance = distance_x
 
-            dictValues = {'distance': distance, 'x': distance_x, 'y': distance_y}
+            dictValues = {'distance': distance, 'x': distance_x,
+                          'y': distance_y}
             distances.append(dictValues)
 
-        # Check if y coordinates match
+        # Check if y coordinates match.
         elif((distance_x is False) and (distance_y) is not False):
             distance = distance_y
 
-            dictValues = {'distance': distance, 'x': distance_x, 'y': distance_y}
+            dictValues = {'distance': distance, 'x': distance_x,
+                          'y': distance_y}
             distances.append(dictValues)
 
-        # x and y coordinates don't match so use Pythagoras to calculate distance
+        # x and y coord don't match so use Pythagoras to calculate distance.
         elif((distance_x is not False) and (distance_y is not False)):
             # Pythagoras
-            distance = round(math.sqrt(pow(distance_x, 2) + pow(distance_y, 2)), 4)
+            distance = round(math.sqrt(pow(distance_x, 2) +
+                             pow(distance_y, 2)), 4)
 
-            dictValues = {'distance': distance, 'x': distance_x, 'y': distance_y}
+            dictValues = {'distance': distance, 'x': distance_x,
+                          'y': distance_y}
             distances.append(dictValues)
 
-        # Set distance to itself to 0:
+        # Set distance to itself to 0.
         elif((distance_x is False) and (distance_y is False)):
             distance = 0
 
-            dictValues = {'distance': distance, 'x': distance_x, 'y': distance_y}
+            dictValues = {'distance': distance, 'x': distance_x,
+                          'y': distance_y}
             distances.append(dictValues)
 
     return distances
 
-def getMinimum(distances = []):
+
+def getMinimum(distances=[]):
     minimum = 1000
     place = 0
     for i in range(len(distances)):
@@ -205,18 +210,21 @@ for h in range(len(houses)):
 
     # Get minimum distance and nr of that house in the houses[]-list
     minimumDistance, position = getMinimum(houses[h].distanceToOthers)
-    # Returns amount of decimalpoints
+    # Returns amount of decimalpoints.
     decimalPointsOfMinimum = str(minimumDistance)[::-1].find('.')
 
     # Makes sure water has no freespace
     if houses[h].freespace is not 0:
-        # If minimum has more than 2 decimalpoints, then distance if calculated using Pythagoras
+        # If minimum has more than 2 decimals, distance is calct using Pyth.
         if (decimalPointsOfMinimum > 2):
-            # take the smallest distance as largest possible freespace
-            if houses[h].distanceToOthers[position]['x'] < houses[h].distanceToOthers[position]['y']:
-                houses[h].extraFreespace = houses[h].distanceToOthers[position]['y']
+            # Take the smallest distance as largest possible freespace.
+            if houses[h].distanceToOthers[position]['x'] < \
+               houses[h].distanceToOthers[position]['y']:
+                houses[h].extraFreespace = \
+                    houses[h].distanceToOthers[position]['y']
             else:
-                houses[h].extraFreespace = houses[h].distanceToOthers[position]['x']
+                houses[h].extraFreespace = \
+                    houses[h].distanceToOthers[position]['x']
         else:
             houses[h].extraFreespace = minimumDistance
     else:
