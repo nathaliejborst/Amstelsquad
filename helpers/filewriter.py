9d3c9b3342185e9de6dace93b4bc21983afa116d
bobhamelers <bhamelers@hotmail.com>
2017-12-18 15:11:26 +0100

    Filewriter file comments
    Add comments and functions for the filewriter.

--- helpers/filewriter.py
+++ helpers/filewriter.py
@@ -1,35 +1,46 @@
-# Import csv package
+# Filename: filewriter.py
+# Authors: Nathalie Borst, Dennis Broekhuizen, Bob Hamelers
+#
+# Description: Writes values or coördinates from houses or water to csv files.
+
 import csv
 
 
-'''saveValue'''
-
-
 def saveValue(totalvalue):
+    """Function to save values of the grid in values.csv."""
+
     with open("values.csv", "a") as filewriter:
         fieldnames = ["value"]
         writer = csv.DictWriter(filewriter, fieldnames=fieldnames)
         writer.writerow({"value": totalvalue})
-#
-#
-# '''writeHousesToFile'''
-#
-#
-# def writeHousesToFile():
-#     with open('coordinatesHouses.csv', 'w', newline='') as filewriter:
-#         fieldnames = ['x', 'y', 'width', 'value']
-#         writer = csv.DictWriter(filewriter, fieldnames=fieldnames)
-#         writer.writerow({'value': grid.value})
-#         for house in grid.housesList:
-#             writer.writerow({'x': house.x, 'y': house.y, 'width': house.width})
-#
-#
-# '''writeWaterbodiesToFile'''
-#
-#
-# def writeWaterbodiesToFile():
-#     with open('coordinatesWaterbodies.csv', 'w', newline='') as filewriter:
-#         fieldnames = ['x', 'y', 'width', 'height']
-#         writer = csv.DictWriter(filewriter, fieldnames=fieldnames)
-#         for body in grid.waterBodiesList:
-#             writer.writerow({'x': body.x, 'y': body.y, 'width': body.width, 'height': body.height})
+
+
+def saveBeginValue(gridvalue):
+    """Function to save begin values of the grid in beginValues.csv."""
+
+    with open('beginValues.csv', 'a', newline='') as filewriter:
+        fieldnames = ["value"]
+        writer = csv.DictWriter(filewriter, fieldnames=fieldnames)
+        writer.writerow({'value': gridvalue})
+
+
+def writeHousesToFile(grid):
+    """Function to save all houses coördinates in coordinatesHouses.csv."""
+
+    with open('coordinatesHouses.csv', 'w', newline='') as filewriter:
+        fieldnames = ['x', 'y', 'width', 'value']
+        writer = csv.DictWriter(filewriter, fieldnames=fieldnames)
+        writer.writerow({'value': grid.value})
+        for house in grid.housesList:
+            writer.writerow({'x': house.x, 'y': house.y, 'width': house.width})
+
+
+def writeWaterbodiesToFile(grid):
+    """Function to save water coördinates in coordinatesWaterbodies.csv."""
+
+    with open('coordinatesWaterbodies.csv', 'w', newline='') as filewriter:
+        fieldnames = ['x', 'y', 'width', 'height']
+        writer = csv.DictWriter(filewriter, fieldnames=fieldnames)
+        for body in grid.waterBodiesList:
+            writer.writerow({'x': body.x, 'y': body.y, 'width': body.width,
+                             'height': body.height})
