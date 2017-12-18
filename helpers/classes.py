bf78240e9183c5911ad056aa55e53b6676449390
Dennis Broekhuizen <dennisbroekhuizen@hotmail.com>
2017-12-18 16:04:06 +0100

    Cleaned up files.

--- helpers/classes.py
+++ helpers/classes.py
@@ -1,17 +1,15 @@
-# Import packages
-import numpy as np
-import math
+# Filename: classes.py
+# Authors: Nathalie Borst, Dennis Broekhuizen, Bob Hamelers
+#
+# Description: Declaration of all classes of the solution.
 
-# Set the area variant
+# Declare global variable to use areaVariant in two classes.
 areaVariant = 1
 
 
-'''Grid class'''
-
-
-# Declare elements of grid in class
 class Grid:
-    def __init__(self, housesList = [], waterBodiesList = []):
+    """Declaration of elements in the grid."""
+    def __init__(self, housesList=[], waterBodiesList=[]):
         self.housesList = housesList
         self.waterBodiesList = waterBodiesList
         self.areaWidth = 180
@@ -22,17 +20,16 @@ class Grid:
         value = 0
         for house in self.housesList:
             value += house.value
-            value += (house.extraFreespace - house.freespace) * house.valueUpdate * house.value
+            value += (house.extraFreespace - house.freespace) * \
+                house.valueUpdate * house.value
         self.value = round(value, 2)
         return self.value
 
 
-'''House class'''
-
-
-# Declare elements of house in class
-class House():
-    def __init__(self, width, height, freespace, value, valueUpdate, x=0, y=0, color=None, distanceToOthers = [], extraFreespace=0):
+class House:
+    '''Declaration of elements of a house.'''
+    def __init__(self, width, height, freespace, value, valueUpdate, x=0, y=0,
+                 color=None, distanceToOthers=[], extraFreespace=0):
         self.width = width
         self.height = height
         self.freespace = freespace
@@ -40,10 +37,6 @@ class House():
         self.valueUpdate = valueUpdate
         self.x = x
         self.y = y
-        self.Xmin = self.x
-        self.Xmax = self.x + self.width
-        self.Ymin = self.y
-        self.Ymax = self.y + self.height
         self.color = color
         self.distanceToOthers = [None] * (areaVariant * 20)
         self.extraFreespace = extraFreespace
@@ -54,26 +47,17 @@ class House():
     def area(self):
         return self.width * self.height
 
-
     def Xmint(self):
         return self.x
+
     def Xmaxt(self):
         return self.x + self.width
+
     def Ymint(self):
         return self.y
+
     def Ymaxt(self):
         return self.y + self.height
-
-
-    #
-    # def Xmin(self, x):
-    #     return x
-    # def Xmax(self, x):
-    #     return (x + self.width)
-    # def Ymin(self, y):
-    #     return y
-    # def Ymax(self, y):
-    #     return (y + self.height)
 
     def corners(self, x, y):
         bottomLeft = [x, y]
@@ -85,25 +69,12 @@ class House():
     def spacecorners(self, x, y):
         bottomLeft = [x - self.freespace, y - self.freespace]
         upperLeft = [x - self.freespace, (y + self.height + self.freespace)]
-        upperRight = [(x + self.width + self.freespace), (y + self.height + self.freespace)]
+        upperRight = [(x + self.width + self.freespace), (y + self.height +
+                      self.freespace)]
         bottomRight = [(x + self.width + self.freespace), y - self.freespace]
         return [bottomLeft, upperLeft, upperRight, bottomRight]
 
     def extraFreeSpaceCorners(self, x, y):
-        # Adjusts freespace if the bottom side of the freespace crosses the left border of the grid (x < 0).
-        # if x - self.extraFreespace < 0:
-        #     self.extraFreespace = x
-        # # CAdjusts freespace if the right side of the freespace crosses the right border of the grid (x > 180).
-        # if x + self.width +  self.extraFreespace > 180:
-        #     self.extraFreespace = 180 - x - self.width
-        # # Adjusts freespace if the left side of the freespace crosses the bottom of the grid (y < 0).
-        # if y - self.extraFreespace < 0:
-        #     self.extraFreespace = y
-        # # Adjusts freespace if the top side of the freespace crosses the bottom border of the grid (y > 160).
-        # if y + self.height + self.extraFreespace > 160:
-        #     self.extraFreespace = 160 - y - self.height
-
-        # Adjust corners of freespace to the new value because of the limitations of the border of the grid.
         leftX = x - self.extraFreespace
         rightX = x + self.width + self.extraFreespace
         bottomY = y - self.extraFreespace
@@ -130,11 +101,8 @@ class House():
         return self.height + self.freespace * 2
 
 
-'''Water class'''
-
-
-# Declare elements of water in class
 class Water:
+    '''Declaration of elements of the water.'''
     def __init__(self):
         self.totalSurface = 5670
         self.width = 0
@@ -154,9 +122,12 @@ class Water:
 
     def Xmin(self, x):
         return x
+
     def Xmax(self, x):
         return (x + self.width)
+
     def Ymin(self, y):
         return y
+
     def Ymax(self, y):
         return (y + self.height)
--- main.py
+++ main.py
@@ -1,125 +1,129 @@
 # Filename: main.py
 # Authors: Nathalie Borst, Dennis Broekhuizen, Bob Hamelers
 #
-# Description: Plots potential residential area Amstelhaege.
-
-# Import packages and own files
+# Description: Main file of the solution to solve the Amstelhaege case. Ask the
+# user for input. Declare variables according to input. Call functions from
+# helpers to run a possible solution and show a visualisation to the user.
 
 from helpers import classes as cl
-from datetime import datetime
-from helpers import visualize as vs
-from helpers import MinimumDistance as md
 from helpers import PlaceAndIntersect as pai
-from helpers import filewriter as fw
+from helpers import MinimumDistance as md
 from helpers import algorithms as al
-import csv
+from helpers import filewriter as fw
 
-def userAreaVariantInput():
+
+def userInput():
+    """Ask the user for input and check if the input is valid."""
+    # Let user choose area variant (20, 40 or 60 houses).
     while True:
         try:
             areaVariant = int(input("Area variant (nr. between 1 to 3): "))
+            if areaVariant < 1 or areaVariant > 3:
+                print("   Please enter a number between 1 to 3")
+                continue
+        except ValueError:
+            print("   This is not a whole number.")
+            continue
+        break
+
+    # Let user choose an algrorithm.
+    while True:
+        try:
             print(" 1: Hillclimber")
             print(" 2: Hillclimber combined with swap houses")
             print(" 3: Simulated annealing")
             print(" 4: Combine all")
             chosenAlgorithm = int(input("Choose an algrorithm: "))
-            repeatAlgorithm = int(input("How many times repeat the algorithm?: "))
-            # if chooseAlgo is 1:
-            #     repeatHillclimber = int(input("Repeat hillclimber: "))
-            #     repositionHouse =  int(input(" > Reposition 1 house: "))
+            if chosenAlgorithm < 1 or chosenAlgorithm > 4:
+                print("   Please enter a number between 1 to 4")
+                continue
         except ValueError:
             print("   This is not a whole number.")
             continue
-        if areaVariant < 1 or areaVariant > 3 or chosenAlgorithm < 1 or chosenAlgorithm > 4:
-            print("   Please enter a number between 1 to 3")
+        break
+
+    # Let user choose how many times it wants to repeat the chosen algorithm.
+    while True:
+        try:
+            repeatAlgorithm = \
+                int(input("How many times repeat the algorithm?: "))
+            if repeatAlgorithm < 0:
+                print("   Please enter a number between 1 to 4")
+                continue
+        except ValueError:
+            print("   This is not a whole number.")
             continue
-        # if repeatHillclimber < 1 or repositionHouse < 1:
-        #     print("   Please enter a positive number")
-        #     continue
-        else:
-            return areaVariant, chosenAlgorithm, repeatAlgorithm
-            break
+        break
 
-cl.areaVariant, chosenAlgorithm, repeatAlgorithm = userAreaVariantInput()
+    return areaVariant, chosenAlgorithm, repeatAlgorithm
 
-# DateTime runner
-startTime = datetime.now()
 
-# Fixing random state for reproducibility
-# np.random.seed(2)
-
-# Create instance of the grid
+# Create instance of the grid.
 grid = cl.Grid()
 
-# Declare water and possible area variants by setting matching amount of housetypes
-# Choose area variant by changing areaVariant to 1, 2 or 3.
-areaVariant = 1
+# Save userinput.
+cl.areaVariant, chosenAlgorithm, repeatAlgorithm = userInput()
 
-# Maisons minus 1, because first maison is placed manually
+# Declaration of variables of the water. Total surface = 5760.
+# Manually add value(s) for waterbodies.
+waterWidth = [144]
+waterHeight = [40]
+waterX = [18]
+waterY = [20]
+
+# Declaration of the number of elements in grid according to area variant.
+# Maisons minus 1, because first maison is placed manually.
 amountOfMaisons = (3 * cl.areaVariant) - 1
 amountOfBungalows = 5 * cl.areaVariant
 amountOfFamilyHouses = 12 * cl.areaVariant
-amountOfWater = 1
-totalWaterSurface = 5760
+amountOfWater = len(waterWidth)
 
-# To test, place bodies of water manually in the grid
-width = [144, 100]
-height = [40, 10]
-x = [18, 80]
-y = [20, 120]
-
+# Place water to the grid.
 for i in range(amountOfWater):
     water = cl.Water()
-    water.width = width[i]
-    water.height = height[i]
-    water.x = x[i]
-    water.y = y[i]
+    water.width = waterWidth[i]
+    water.height = waterHeight[i]
+    water.x = waterX[i]
+    water.y = waterY[i]
     grid.waterBodiesList.append(water)
-    # waterBodies.append(water)
 
-# water = cl.Water()
-# water.width = 144
-# water.height = 40
-# water.x = 18
-# water.y = 60
-# waterBodies.append(water)
-
-# Place first maison
+# Place first maison manually.
 pai.placeFirstMaison(cl.House(width=11, height=10.5, freespace=6, value=610000,
                      valueUpdate=0.06), grid)
 
-# Place bungalows
+# Place bungalows.
 for bungalows in range(amountOfBungalows):
     bungalow = cl.House(width=10, height=7.5, freespace=3, value=399000,
                         valueUpdate=0.04, color='orange')
-    # Tries to place house at the waterside, else places it random in the grid (no priority: call pai.placeHouse() instead)
+    # Try to place house at waterside, else place it random in grid.
     pai.placeHouseWithWatersidePriority(bungalow, grid)
 
-# Place maisons
+# Place maisons.
 for maisons in range(amountOfMaisons):
     maison = cl.House(width=11, height=10.5, freespace=6, value=610000,
                       valueUpdate=0.06, color='red')
-    # Tries to place house at the waterside, else places it random in the grid (no priority: call pai.placeHouse() instead)
+    # Try to place house at waterside, else place it random in grid.
     pai.placeHouseWithWatersidePriority(maison, grid)
 
-# Place familyhouses
+# Place familyhouses.
 for familyHouses in range(amountOfFamilyHouses):
     familyHouse = cl.House(width=8, height=8, freespace=2, value=285000,
                            valueUpdate=0.03, color='yellow')
-    # Tries to place house at the waterside, else places it random in the grid (no priority: call pai.placeHouse() instead)
+    # Try to place house at waterside, else place it random in grid.
     pai.placeHouseWithWatersidePriority(familyHouse, grid)
 
-# Call to MinimumDistance.py file with grid for add extra freespace
+# Add extra freespace to all houses added to grid.
 md.addExtraFreespaceToAllHouse(grid)
 
-# Two printstatements for the total value and total runtime
-print("Total value: {}".format(round(grid.totalValue(), 2)))
-print("Total runtime: {}".format(datetime.now() - startTime))
+# Write begin value of grid to csv.
+fw.saveBeginValue(grid.totalValue())
 
-# The total value of the grid
-grid.totalValue()
+# Show begin value to user and give instructions to continue.
+print("Begin value: {}".format(round(grid.totalValue(), 2)))
+print("Close plot window to start the algorithm")
 
+# Run algorithms according to user input to improve grid value.
 al.improveValue(grid, chosenAlgorithm, repeatAlgorithm)
 
-# Call to saveValue function in filewriter.py file with grid.value
+# Write final value of grid to csv.
 fw.saveValue(grid.value)
